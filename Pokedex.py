842e48ed4fc03d8a7af2bbac7a5357f28231f896
wallingson <128642980+wallingson12@users.noreply.github.com>
2025-08-17 14:27:06 -0300

    Update Pokedex.py

--- Pokedex.py
+++ Pokedex.py
@@ -10,7 +10,7 @@ def obter_dados_pokemon(nome_pokemon):
         return response.json()
     return None
 
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template('pokedex.html', pokemon=None, message=None)
 
